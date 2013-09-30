618f580b1f26477fb600bfcdefb5ceea8ed4e079
contact-banker <contact-banker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-30 07:01:26 +0000

    Updated license info.
    git-svn-id: https://plugins.svn.wordpress.org/captcha-bank/trunk@780179 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- captcha_bank_image.php
+++ captcha_bank_image.php
@@ -1,4 +1,47 @@
 <?php 
+/**
+ * Project:     Securimage: A PHP class for creating and managing form CAPTCHA images<br />
+ * File:        securimage.php<br />
+ *
+ * Copyright (c) 2013, Drew Phillips
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Any modifications to the library should be indicated clearly in the source code
+ * to inform users that the changes are not a part of the original software.<br /><br />
+ *
+ * If you found this script useful, please take a quick moment to rate it.<br />
+ * http://www.hotscripts.com/rate/49400.html  Thanks.
+ *
+ * @link http://www.phpcaptcha.org Securimage PHP CAPTCHA
+ * @link http://www.phpcaptcha.org/latest.zip Download Latest Version
+ * @link http://www.phpcaptcha.org/Securimage_Docs/ Online Documentation
+ * @copyright 2013 Drew Phillips
+ * @author Drew Phillips <drew@drew-phillips.com>
+ * @version 3.5.1 (June 21, 2013)
+ * @package Securimage
+ *
+ */
 
 class captcha_bank_image
 {
@@ -254,7 +297,7 @@ class captcha_bank_image
 		$this->noise_level = $options[12] == 1 ? $options[13] : 0;
 		$this->use_transparent_text = $options[15] == 1 ? true : false;
 		$this->text_transparency_percentage = $options[16];
-		$this->image_signature = $options[20] == 1 ?  $options[21] : '';
+		$this->image_signature = $options[20] == 1 ?  stripslashes($options[21]) : '';
 		$this->case_sensitive = $options[8] == 1 ?  true : false;
 		if($options[17] == "digits")
 		{
