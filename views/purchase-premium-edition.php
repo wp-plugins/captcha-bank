a5fa458e64c26232b3846cfc79a4872bf8f813d5
contact-banker <contact-banker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-30 07:41:59 +0000

    New Version Release
    git-svn-id: https://plugins.svn.wordpress.org/captcha-bank/trunk@1078795 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/purchase-premium-edition.php
+++ views/purchase-premium-edition.php
@@ -791,7 +791,7 @@ else
 														<?php _e("You just need to pay for once for life time.", captcha_bank); ?>
 													</span>
 													 <h1 class="col1">
-														&euro;<span><?php _e("11", captcha_bank); ?></span>
+														&euro;<span><?php _e("13", captcha_bank); ?></span>
 													</h1>
 													<h3 class="col1">
 														<?php _e("one time", captcha_bank); ?>
@@ -1105,7 +1105,7 @@ else
 														<?php _e("You just need to pay for once for life time.", captcha_bank); ?>
 													</span>
 													<h1 class="col1">
-														&euro;<span><?php _e("17", captcha_bank); ?></span>
+														&euro;<span><?php _e("20", captcha_bank); ?></span>
 													</h1>
 													<h3 class="col1">
 														<?php _e("one time", captcha_bank); ?>
